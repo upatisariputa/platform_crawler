66c8dfc9792be896205fd8fdd34e476e967d10dd
sitaruta <sitaruta@sitarutaui-MacBookPro.local>
2019-11-18 02:44:46 +0900

    Make twitch module

--- twitch/get_user_id.py
+++ twitch/get_user_id.py
@@ -2,32 +2,28 @@ import requests
 from clientID import client_id
 from http_data import url, headers
 from twitch_bj_name_data import bj_names_datas
+# from twitch_bj_id_data import bj_ids_datas
 
 def get_user_id(names = bj_names_datas): #매개변수에 기본값을 정의하고 싶을 경우 '='을 사용하여 기본값을 정해준다.
-    bj_ids = []
+    bj_ids_datas = []
     if isinstance(names, list): # 인자가 list인지 아닌지 판별하여 list 이면 반복문을 실행한다.
         for name in names:
-            r = requests.get(url + 'users?login=' + name, headers=headers)
-            bj_id = r.json()
+            res = requests.get(url + 'users?login=' + name, headers=headers)
+            bj_id = res.json()
             print(name)
             if bj_id == {'data': []} or bj_id is None:
-                bj_ids.append('none')
+                bj_ids_datas.append('none')
             else:
                 id = bj_id['data'][0]['id']
-                bj_ids.append(id)
+                bj_ids_datas.append(id)
+            print(bj_ids_datas)
     else:
-        r = requests.get(url + 'users?login=' + names, headers=headers)
-        bj_id = r.json()
+        res = requests.get(url + 'users?login=' + names, headers=headers)
+        bj_id = res.json()
         if bj_id == {'data': []} or bj_id is None:
             pass # if문에서 아무것도 안 하고 싶을 경우 pass를 사용한다.
         else:
             id = bj_id['data'][0]['id']
-            bj_ids.append(id)
-    print(bj_ids)
-
-
-
-
+            bj_ids_datas.append(id)
+    
 get_user_id()
-
-# get_user_id('magenta62')
--- twitchAPI.py
+++ twitchAPI.py
@@ -1,25 +1,5 @@
-# client_id = 'gflzb7us8z6x634sd4fxsyg36qofi4'
-# headers = {'Client-ID' : client_id, 'referer' : 'https://twitch.tv'}
-# # referer란? Referer 요청 헤더는 현재 요청된 페이지의 링크 이전의 웹페이지 주소를 포함합니다. Referer 헤더는 사람들이 어디로부터 와서 방문 중인지를 인식할 수 있도록 해줍니다. 
-# # (https://developer.mozilla.org/ko/docs/Web/HTTP/Headers/Referer)
+from twitch.twitch_bj_info_data import twitch_bj_info_data
+from twitch.twitch_bj_videos_data import twitch_bj_videos_data
+from twitch.twitch_bj_follower_data import twitch_bj_follower_data
 
-# # bjinfo 가져오기
-# URL = 'https://api.twitch.tv/helix/users?id='+ bjID
-
-# req = Request(URL, headers = headers)
-# bjInfo = req.json()
-#         image_url = user['data'][0]['profile_image_url']
-#         user_name = user['data'][0]['display_name']
-#         user_info = user['data'][0]['description']
-
-import requests
-from twitch.clientID import client_id
-
-url = 'https://api.twitch.tv/helix/users?id=' + '275644615'
-headers = {'Client-ID' : client_id, 'referer' : 'https://twitch.tv'}
-res = requests.get(url = url, headers=headers)
-bj_info = res.json()
-bj_name = bj_info['data'][0]['display_name']
-bj_image = bj_info['data'][0]['offline_image_url']
-bj_platfrom = 'twitch'
-print(bj_name, bj_image)
+# 데이터 베이스에 넣는걸 만들기
