d77f06bb80e9528e08f34e71fd8cc7162b862ebb
sitaruta <sitaruta@sitarutaui-MacBookPro.local>
2019-11-25 23:02:07 +0900

    Delete trash flie

--- twitchAPI.py
+++
@@ -1,5 +0,0 @@
-from twitch.twitch_bj_info_data import twitch_bj_info_data
-from twitch.twitch_bj_videos_data import twitch_bj_videos_data
-from twitch.twitch_bj_follower_data import twitch_bj_follower_data
-
-# 데이터 베이스에 넣는걸 만들기
