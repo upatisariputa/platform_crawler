35a4fe8cb44bc380a054f86564600765a5aa8952
sitaruta <sitaruta@sitarutaui-MacBookPro.local>
2019-11-25 18:05:48 +0900

    Make youtube handler and seperate test.py

--- youtube/get_from_youtube_api.py
+++ youtube/get_from_youtube_api.py
@@ -4,7 +4,7 @@ from youtube_api_key import api_key_1
 def get_from_youtube_api(id):
     name = id
 
-    base_url = "https://www.googleapis.com/youtube/v3/channels?part=snippet&part=statistics&id="
+    base_url = "https://www.googleapis.com/youtube/v3/channels?part=statistics&id="
 
     key = api_key_1
 
@@ -15,8 +15,8 @@ def get_from_youtube_api(id):
     video_cnt = data['items'][0]['statistics']['videoCount']
     view_cnt = data['items'][0]['statistics']['viewCount']
     # info = []
-    print(data)
+    print(subscriber_cnt, view_cnt, video_cnt)
 
-get_from_youtube_api('UC3SyT4_WLHzN7JmHQwKQZww')
+# get_from_youtube_api('UCu750LH-nGQetXoosDwcWOg')
 
 # https://github.com/skfn20/webscraping/blob/9a5147d9b312536b56afefda821c1a857f6c6459/youtubeScraper/youtube_Title_subscribe.py
